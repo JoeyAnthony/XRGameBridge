3e3d9cff5fdc12ade6955f80afe833ad49959fa5
Joey <jvanhoek@hotmail.com>
2025-02-16 15:04:58 +0100

    Log usage of debug device

--- runtime_openxr/src/instance.cpp
+++ runtime_openxr/src/instance.cpp
@@ -283,6 +283,7 @@ XrResult xrGetD3D12GraphicsRequirementsKHR(XrInstance instance, XrSystemId syste
 
 //#ifdef _DEBUG
 //    // Enable the D3D12 debug layer.
+//    LOG(WARNING) << "DirectX 12 Debug device is being used";
 //    {
 //        ComPtr<ID3D12Debug> debugController;
 //        if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(debugController.GetAddressOf())))) {
